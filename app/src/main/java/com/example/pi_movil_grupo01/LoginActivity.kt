3ec8f65b386e89a9d9ef8ce5930a6b7d2c975162
ChrisRP <i202212697@cibertec.edu.pe>
2024-11-17 20:52:49 -0500

    agregando api authservice login activity

--- app/src/main/java/com/example/pi_movil_grupo01/LoginActivity.kt
+++ app/src/main/java/com/example/pi_movil_grupo01/LoginActivity.kt
@@ -1,17 +1,56 @@
 package com.example.pi_movil_grupo01
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pi_movil_grupo01.entity.AuthRequest
+import com.example.pi_movil_grupo01.entity.AuthResponse
+import com.example.pi_movil_grupo01.service.AuthService
+import com.example.pi_movil_grupo01.util.ApiClient
+import com.example.pi_movil_grupo01.util.PreferenceHelper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tcGoRegister: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
 
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btn_go_to_menu)
+        tcGoRegister = findViewById(R.id.tv_go_to_register)
+
+        val preferences: SharedPreferences = PreferenceHelper.defaultPrefs(this)
+        if (preferences.getBoolean("session", false)) {
+            goToMenu()
+        }
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+
+        tcGoRegister.setOnClickListener {
+            goToRegister()
+        }
 
 
 
@@ -21,4 +60,54 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
     }
+
+
+
+    private fun login() {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Por favor ingresa tus credenciales", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val request = AuthRequest(email, password)
+
+        val service = ApiClient.getClient(this).create(AuthService::class.java)
+        service.login(request).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                if (response.isSuccessful && response.body() != null) {
+                    val token = response.body()!!.token
+
+                    PreferenceHelper.defaultPrefs(this@LoginActivity).edit()
+                        .putString("auth_token", token)
+                        .putBoolean("session", true)
+                        .apply()
+
+                    Toast.makeText(this@LoginActivity, "Login exitoso", Toast.LENGTH_SHORT).show()
+                    goToMenu()
+                } else {
+                    Toast.makeText(this@LoginActivity, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Toast.makeText(this@LoginActivity, "Error de red", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun goToRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToMenu() {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/example/pi_movil_grupo01/util/ApiClient.kt
+++ app/src/main/java/com/example/pi_movil_grupo01/util/ApiClient.kt
@@ -1,4 +1,29 @@
 package com.example.pi_movil_grupo01.util
 
+import android.content.Context
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiClient {
+    private const val BASE_URL = "https://athletic-connection-prueba00.up.railway.app/"
+    private var retrofit: Retrofit? = null
+
+
+    fun getClient(context: Context): Retrofit {
+        if (retrofit == null) {
+            val client = OkHttpClient.Builder()
+                .addInterceptor(AuthInterceptor(context))
+                .build()
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit!!
+    }
+
+
 }
